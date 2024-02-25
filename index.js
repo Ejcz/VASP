0b0f954fe9875c073ddd3bd2e72dbd69154d161d
Arkantanti <kubson.pieczonka@gmail.com>
2024-02-25 16:55:07 +0100

    map drag rendering

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-// disabling right click
+//disabling right click
 
 document.addEventListener('contextmenu', (event) => {
 	event.preventDefault();
 });
 
-//onload hex generating:
+//global variables
 
 const hex_rows = 15;
 const hex_columns = 15;
 
-const mapa = document.querySelector('.map-drag');
-for (let i = 1; i <= hex_rows; i++) {
-	for (let j = 1; j <= hex_columns; j++) {
-		mapa.insertAdjacentHTML('beforeend', '<div class="hex-border"><div class="hex">' + i + ',' + j + '</div></div>');
-	}
-	mapa.insertAdjacentHTML('beforeend', '<br />');
-	if (i % 2 == 1) {
-		mapa.insertAdjacentHTML('beforeend', '<div class="hex-space"></div>');
-	}
-}
-
-//map dragging:
+var current_row = 0;
+var current_column = 0;
 
 const map_drag = document.querySelector('.map-drag');
 const map_supp = document.querySelector('.map-supp');
+
+map_drag.insertAdjacentHTML('beforeend', '<div class="hex"></div>');
 
 const hex_margin = parseFloat(window.getComputedStyle(document.querySelector('.hex')).margin);
 const hex_width = parseFloat(window.getComputedStyle(document.querySelector('.hex')).width);
@@ -31,17 +23,31 @@ const hex_height = parseFloat(window.getComputedStyle(document.querySelector('.h
 const map_height = parseFloat(window.getComputedStyle(map_supp).height);
 const map_width = parseFloat(window.getComputedStyle(map_supp).width);
 
-console.log(hex_width);
+const hex_wn = Math.ceil(map_width / (hex_width + 2 * hex_margin)) + 3;
+const hex_hn = Math.ceil((map_height - hex_height / 4) / (2 * hex_margin + 0.75 * hex_height)) + 3;
 
-const left_limit = 15;
-const top_limit = 15;
-const right_limit = -1 * (15 + hex_columns * hex_width + (hex_columns + 1) * hex_margin * 2 - map_width + hex_width * 0.5);
-const bottom_limit = -1 * (15 + hex_rows * hex_height * 0.75 + hex_rows * 2 * hex_margin + hex_height * 0.25 - map_height);
+//hex rendering function
 
-map_drag.style.top = `${bottom_limit / 2}px`;
-map_drag.style.left = `${right_limit / 2}px`;
+function hex_gen(row, col) {
+	map_drag.innerHTML = '';
+	for (let i = 0; i < hex_hn; i++) {
+		for (let j = 0; j < hex_wn; j++) {
+			let id_x = (((row + i) % hex_rows) + hex_rows) % hex_rows;
+			let id_y = (((col + j) % hex_columns) + hex_columns) % hex_columns;
+			map_drag.insertAdjacentHTML('beforeend', '<div class="hex" id="' + id_x + ',' + id_y + '">' + id_x + ',' + id_y + '</div>');
+		}
+		map_drag.insertAdjacentHTML('beforeend', '<br />');
+		if (i % 2 == ((current_row % 2) + 2) % 2) {
+			map_drag.insertAdjacentHTML('beforeend', '<div class="hex-space"></div>');
+		}
+	}
+}
 
-console.log(right_limit);
+hex_gen(current_row, current_column);
+map_drag.style.top = `${-2 * hex_height - 4 * hex_margin}px`;
+map_drag.style.left = `${-2 * hex_height - 4 * hex_margin}px`;
+
+//map dragging:
 
 function onMouseDrag({ movementX, movementY }) {
 	let getStyle = window.getComputedStyle(map_drag);
@@ -49,13 +55,32 @@ function onMouseDrag({ movementX, movementY }) {
 	let topValue = parseInt(getStyle.top);
 	let outX = leftValue + movementX;
 	let outY = topValue + movementY;
-	if (left_limit >= outX && outX >= right_limit) {
-		map_drag.style.left = `${outX}px`;
-	}
-	if (top_limit >= outY && outY >= bottom_limit) {
+
+	if (outY > -1 * hex_height - 2 * hex_margin) {
+		current_row -= 1;
+		hex_gen(current_row, current_column);
+		map_drag.style.top = `${outY - 0.75 * hex_height - 2 * hex_margin}px`;
+	} else if (outY < map_height - (hex_hn - 2) * (hex_height * 0.75 + 2 * hex_margin) - hex_height - 2 * hex_margin) {
+		current_row += 1;
+		hex_gen(current_row, current_column);
+		map_drag.style.top = `${outY + 0.75 * hex_height + 2 * hex_margin}px`;
+	} else {
 		map_drag.style.top = `${outY}px`;
 	}
+
+	if (outX > -1 * hex_width - 2 * hex_margin) {
+		current_column -= 1;
+		hex_gen(current_row, current_column);
+		map_drag.style.left = `${outX - hex_width - 2 * hex_margin}px`;
+	} else if (outX < map_width - (hex_wn - 1) * (hex_width + 2 * hex_margin)) {
+		current_column += 1;
+		hex_gen(current_row, current_column);
+		map_drag.style.left = `${outX + hex_width + 2 * hex_margin}px`;
+	} else {
+		map_drag.style.left = `${outX}px`;
+	}
 }
+
 map_supp.addEventListener('mousedown', (ev) => {
 	if (ev.button == '2') {
 		map_supp.addEventListener('mousemove', onMouseDrag);
